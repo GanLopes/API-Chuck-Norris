b64bb7e8d1fb376f2ef8f960af81020bae0a6401
CodeSandbox <support@codesandbox.io>
2023-10-21 19:50:43 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,93 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [piada, setPiada] = useState("");
+  const [favoritos, setFavoritos] = useState([]);
+
+  useEffect(() => {
+    const favoritosSalvos = JSON.parse(localStorage.getItem("favoritos"));
+    if (favoritosSalvos) {
+      setFavoritos(favoritosSalvos);
+    }
+  }, []);
+
+  useEffect(() => {
+    buscarProximaPiada();
+  }, []);
+
+  const buscarProximaPiada = () => {
+    fetchPiada();
+  };
+
+  const fetchPiada = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => setPiada(data.value))
+      .catch((error) => console.error(error));
+  };
+
+  const curtirPiada = () => {
+    if (!favoritos.includes(piada)) {
+      const novosFavoritos = [...favoritos, piada];
+      setFavoritos(novosFavoritos);
+
+      localStorage.setItem("favoritos", JSON.stringify(novosFavoritos));
+    }
+  };
+
+  const limparFavoritos = () => {
+    setFavoritos([]);
+    localStorage.removeItem("favoritos");
+  };
+
+  const removerFavorito = (index) => {
+    const favoritosAtualizados = [...favoritos];
+    favoritosAtualizados.splice(index, 1);
+    setFavoritos(favoritosAtualizados);
+
+    localStorage.setItem("favoritos", JSON.stringify(favoritosAtualizados));
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
+    <div>
+      <section className="site-info">
+        <header className="page-header">
+        <h1>Piadas que fariam o Chuck Norris sorrir</h1>
+        <hr className="header-separator" />
+        <div className="centered-text">
+    <p>Este site é dedicado às piadas de Chuck Norris, o famoso ator e artista marcial. Chuck Norris é conhecido por suas habilidades excepcionais e pelo humor único associado a ele.</p>
+    <p>Nosso objetivo é proporcionar a você uma experiência divertida, compartilhando algumas das melhores piadas de Chuck Norris.</p>
+    <p>OBS: As piadas estão disponíveis apenas em inglês.</p>
+  </div>
+         </header>
+      </section>
 
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <hr />
+
+      <section className="piada-section">
+        <h2>Piadas:</h2>
+        <p>{piada}</p>
+        <button onClick={curtirPiada}>Curtir a piada</button>
+        <button onClick={buscarProximaPiada}>Conta outra!</button>
+      </section>
+
+      <section className="favoritos-section">
+        <h2>Favoritos</h2>
+        <ul>
+          {favoritos.map((piadaFavorita, index) => (
+            <li key={index}>
+              <div className="favorito-item">
+                <span>{piadaFavorita}</span>
+                <button onClick={() => removerFavorito(index)}>X</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button onClick={limparFavoritos}>Limpar todas as favoritas</button>
+      </section>
     </div>
   );
 }
 
 export default App;
+import "./App.css";
--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+ 
 );
+
+
